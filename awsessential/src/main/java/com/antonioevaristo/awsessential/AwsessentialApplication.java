a0f2bb443961b4643fc3b4b3f7ffbf2bcefc5230
Antonio Evaristo <antonio.c.e.barbosa@gmail.com>
2024-04-23 23:55:13 -0300

    projeto inicial

---
+++ awsessential/src/main/java/com/antonioevaristo/awsessential/AwsessentialApplication.java
@@ -0,0 +1,13 @@
+package com.antonioevaristo.awsessential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsessentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsessentialApplication.class, args);
+	}
+
+}
