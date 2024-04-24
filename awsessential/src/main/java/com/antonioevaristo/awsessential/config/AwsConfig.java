9c2d96ff609a2ec49ef5b91a7499a3d9225d334e
Antonio Evaristo <antonio.c.e.barbosa@gmail.com>
2024-04-24 00:17:43 -0300

    configurando aws

---
+++ awsessential/src/main/java/com/antonioevaristo/awsessential/config/AwsConfig.java
@@ -0,0 +1,32 @@
+package com.antonioevaristo.awsessential.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+    @Value("${aws.access.id}")
+    private String accessKey;
+    @Value("${aws.access.secret}")
+    private String accessSecret;
+    @Value("${aws.s3.region}")
+    private String bucketRegion;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        final BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion(Regions.fromName(bucketRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+}
