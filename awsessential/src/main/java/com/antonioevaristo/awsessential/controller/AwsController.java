731f9e207424f36f919de3c1b721825dcfc5b641
Antonio Evaristo <antonio.c.e.barbosa@gmail.com>
2024-05-03 22:12:46 -0300

    adicionando o rds mysql ao springboot

--- awsessential/src/main/java/com/antonioevaristo/awsessential/controller/AwsController.java
+++ awsessential/src/main/java/com/antonioevaristo/awsessential/controller/AwsController.java
@@ -1,6 +1,7 @@
 package com.antonioevaristo.awsessential.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/aws/s3")
 public class AwsController {
     private final AmazonS3 amazonS3;
-    public AwsController(AmazonS3 amazonS3) {
+    private final JdbcTemplate jdbcTemplate;
+    public AwsController(AmazonS3 amazonS3,JdbcTemplate jdbcTemplate) {
     this.amazonS3 = amazonS3;
+    this.jdbcTemplate = jdbcTemplate;
     }
     @PostMapping("note")
     public void salvarNota(@RequestParam String name,@RequestParam String content){
         amazonS3.putObject("terraformtstaceb", name +".txt", content);
+        jdbcTemplate.update("insert into notes (nome, conteudo) values (?,?)", name, content);
     }
 }
